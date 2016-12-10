feac141f94a6bd4a3bf938510163115b179cef3f
Roman Pokrovskij <roman.pokrovskij@gmail.com>
2016-12-10 22:30:14 +0200

    ver 8

--- CreateNugetPackage.ps1
+++ CreateNugetPackage.ps1
@@ -1,5 +1,5 @@
 # nuget spec
-.\nuget.exe pack Routines.1.0.7.nuspec
+.\nuget.exe pack Routines.1.0.8.nuspec
 
 # delete all except App_Data folder and its content
 # D:\cot\Vse\AdminkaV1\Web\bin\Debug\net461\win7-x64
