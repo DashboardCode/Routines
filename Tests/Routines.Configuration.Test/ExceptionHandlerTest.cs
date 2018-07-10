453bca88e3fa131c5f335dd121e3e175d5c0d673
Roman Pokrovskij <roman.pokrovskij@gmail.com>
2018-07-11 02:01:42 +0300

    nlog migration

--- Tests/Routines.Configuration.Test/ExceptionHandlerTest.cs
+++ Tests/Routines.Configuration.Test/ExceptionHandlerTest.cs
@@ -1,12 +1,12 @@
 ﻿namespace DashboardCode.Routines.Configuration.Test
 {
-#if !(NETCOREAPP1_1 || NETCOREAPP2_0  || NETCOREAPP2_1)
+#if !NETCOREAPP
     [Microsoft.VisualStudio.TestTools.UnitTesting.TestClass]
 #endif
     public partial class ExceptionHandlerTest
     {
 
-#if NETCOREAPP1_1 || NETCOREAPP2_0  || NETCOREAPP2_1
+#if NETCOREAPP
         [Xunit.Fact]
 #else
         [Microsoft.VisualStudio.TestTools.UnitTesting.TestMethod]
