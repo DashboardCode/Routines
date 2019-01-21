b138fbf17e31aac11fb226eee8897f93578b156e
Roman Pokrovskij <roman.pokrovskij@gmail.com>
2019-01-21 03:09:19 +0200

    btf

--- Tests/AdminkaV1.Injected.AspCore.MvcApp.NETCore.Test/AdminkaIntegrationUnitTest.cs
+++ Tests/AdminkaV1.Injected.AspCore.MvcApp.NETCore.Test/AdminkaIntegrationUnitTest.cs
@@ -4,7 +4,6 @@ using System.Net;
 using System.Net.Http;
 using System.Threading.Tasks;
 
-using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.TestHost;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
--- Tests/AdminkaV1.Injected.AspCore.MvcApp.NETCore.Test/TestManager.cs
+++ Tests/AdminkaV1.Injected.AspCore.MvcApp.NETCore.Test/TestManager.cs
@@ -30,45 +30,45 @@ namespace DashboardCode.AdminkaV1.Injected.AspCore.MvcApp.NETCore.Test
             return contentRoot;
         }
 
-        public static void InitializeServices(IServiceCollection services)
-        {
-            services.Configure((RazorViewEngineOptions options) =>
-            {
-                var previous = options.CompilationCallback;
-                options.CompilationCallback = (context) =>
-                {
-                    previous?.Invoke(context);
+        //public static void InitializeServices(IServiceCollection services)
+        //{
+        //    services.Configure((RazorViewEngineOptions options) =>
+        //    {
+        //        var previous = options.CompilationCallback;
+        //        options.CompilationCallback = (context) =>
+        //        {
+        //            previous?.Invoke(context);
 
-                    var assembly = typeof(Startup).GetTypeInfo().Assembly;
-                    var assemblies = assembly.GetReferencedAssemblies().Select(x => MetadataReference.CreateFromFile(Assembly.Load(x).Location))
-                    .ToList();
-                    assemblies.Add(MetadataReference.CreateFromFile(Assembly.Load(new AssemblyName("mscorlib")).Location));
-                    assemblies.Add(MetadataReference.CreateFromFile(Assembly.Load(new AssemblyName("System.Private.Corelib")).Location));
-                    assemblies.Add(MetadataReference.CreateFromFile(Assembly.Load(new AssemblyName("Microsoft.AspNetCore.Razor")).Location));
-                    //assemblies.Add(MetadataReference.CreateFromFile(Assembly.Load(new AssemblyName("System.Linq")).Location));
-                    //assemblies.Add(MetadataReference.CreateFromFile(Assembly.Load(new AssemblyName("System.Threading.Tasks")).Location));
-                    //assemblies.Add(MetadataReference.CreateFromFile(Assembly.Load(new AssemblyName("System.Runtime")).Location));
-                    //assemblies.Add(MetadataReference.CreateFromFile(Assembly.Load(new AssemblyName("System.Dynamic.Runtime")).Location));
-                    //assemblies.Add(MetadataReference.CreateFromFile(Assembly.Load(new AssemblyName("Microsoft.AspNetCore.Razor.Runtime")).Location));
-                    //assemblies.Add(MetadataReference.CreateFromFile(Assembly.Load(new AssemblyName("Microsoft.AspNetCore.Mvc")).Location));
-                    //assemblies.Add(MetadataReference.CreateFromFile(Assembly.Load(new AssemblyName("Microsoft.AspNetCore.Razor")).Location));
-                    //assemblies.Add(MetadataReference.CreateFromFile(Assembly.Load(new AssemblyName("Microsoft.AspNetCore.Mvc.Razor")).Location));
-                    //assemblies.Add(MetadataReference.CreateFromFile(Assembly.Load(new AssemblyName("Microsoft.AspNetCore.Html.Abstractions")).Location));
-                    //assemblies.Add(MetadataReference.CreateFromFile(Assembly.Load(new AssemblyName("System.Text.Encodings.Web")).Location));
-                    context.Compilation = context.Compilation.AddReferences(assemblies);
-                };
-            });
+        //            var assembly = typeof(Startup).GetTypeInfo().Assembly;
+        //            var assemblies = assembly.GetReferencedAssemblies().Select(x => MetadataReference.CreateFromFile(Assembly.Load(x).Location))
+        //            .ToList();
+        //            assemblies.Add(MetadataReference.CreateFromFile(Assembly.Load(new AssemblyName("mscorlib")).Location));
+        //            assemblies.Add(MetadataReference.CreateFromFile(Assembly.Load(new AssemblyName("System.Private.Corelib")).Location));
+        //            assemblies.Add(MetadataReference.CreateFromFile(Assembly.Load(new AssemblyName("Microsoft.AspNetCore.Razor")).Location));
+        //            //assemblies.Add(MetadataReference.CreateFromFile(Assembly.Load(new AssemblyName("System.Linq")).Location));
+        //            //assemblies.Add(MetadataReference.CreateFromFile(Assembly.Load(new AssemblyName("System.Threading.Tasks")).Location));
+        //            //assemblies.Add(MetadataReference.CreateFromFile(Assembly.Load(new AssemblyName("System.Runtime")).Location));
+        //            //assemblies.Add(MetadataReference.CreateFromFile(Assembly.Load(new AssemblyName("System.Dynamic.Runtime")).Location));
+        //            //assemblies.Add(MetadataReference.CreateFromFile(Assembly.Load(new AssemblyName("Microsoft.AspNetCore.Razor.Runtime")).Location));
+        //            //assemblies.Add(MetadataReference.CreateFromFile(Assembly.Load(new AssemblyName("Microsoft.AspNetCore.Mvc")).Location));
+        //            //assemblies.Add(MetadataReference.CreateFromFile(Assembly.Load(new AssemblyName("Microsoft.AspNetCore.Razor")).Location));
+        //            //assemblies.Add(MetadataReference.CreateFromFile(Assembly.Load(new AssemblyName("Microsoft.AspNetCore.Mvc.Razor")).Location));
+        //            //assemblies.Add(MetadataReference.CreateFromFile(Assembly.Load(new AssemblyName("Microsoft.AspNetCore.Html.Abstractions")).Location));
+        //            //assemblies.Add(MetadataReference.CreateFromFile(Assembly.Load(new AssemblyName("System.Text.Encodings.Web")).Location));
+        //            context.Compilation = context.Compilation.AddReferences(assemblies);
+        //        };
+        //    });
 
-            var startupAssembly = typeof(Startup).GetTypeInfo().Assembly;
+        //    var startupAssembly = typeof(Startup).GetTypeInfo().Assembly;
 
-            // Inject a custom application part manager. 
-            // Overrides AddMvcCore() because it uses TryAdd().
-            var manager = new ApplicationPartManager();
-            manager.ApplicationParts.Add(new AssemblyPart(startupAssembly));
-            manager.FeatureProviders.Add(new ControllerFeatureProvider());
-            manager.FeatureProviders.Add(new ViewComponentFeatureProvider());
+        //    // Inject a custom application part manager. 
+        //    // Overrides AddMvcCore() because it uses TryAdd().
+        //    var manager = new ApplicationPartManager();
+        //    manager.ApplicationParts.Add(new AssemblyPart(startupAssembly));
+        //    manager.FeatureProviders.Add(new ControllerFeatureProvider());
+        //    manager.FeatureProviders.Add(new ViewComponentFeatureProvider());
 
-            services.AddSingleton(manager);
-        }
+        //    services.AddSingleton(manager);
+        //}
     }
 }
