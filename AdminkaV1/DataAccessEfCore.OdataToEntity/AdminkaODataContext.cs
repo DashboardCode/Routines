9ee6683f6be465f907a5f46c5c7f014fe657f0d0
Roman Pokrovskij <roman.pokrovskij@gmail.com>
2019-09-24 15:31:27 +0300

    odata

--- AdminkaV1/DataAccessEfCore.OdataToEntity/AdminkaODataContext.cs
+++ AdminkaV1/DataAccessEfCore.OdataToEntity/AdminkaODataContext.cs
@@ -1,36 +1,37 @@
-﻿//using Microsoft.OData.Edm;
-//using OdataToEntity;
-//using OdataToEntity.Db;
-//using System;
-//using System.IO;
-//using System.Threading;
+﻿using System;
+using System.IO;
+using System.Threading;
+using Microsoft.OData.Edm;
+using OdataToEntity;
 
-//namespace DashboardCode.AdminkaV1.DataAccessEfCore.OdataToEntity
-//{
-//    /// <summary>
-//    /// OdataToEntity can't be referenced in Standard lib (only in .NET Framework)
-//    /// More information: https://github.com/voronov-maxim/OdataToEntity
-//    /// </summary>
+using DashboardCode.Routines.Storage.EfCore;
 
-//    public class AdminkaODataContext
-//    {
-//        readonly OeParser parser;
-//        public AdminkaODataContext(string baseUri, IAdminkaOptionsFactory optionsFactory)
-//        {
-//            var dataAdapter = new AdminkaOeEfCoreDataAdapter(optionsFactory);
-//            //Build OData Edm Model
-//            EdmModel edmModel = dataAdapter.BuildEdmModel();
-//            //Create query parser
-//            this.parser = new OeParser(new Uri(baseUri /*"http://dummy"*/), dataAdapter, dataAdapter.BuildEdmModel());
-//        }
+namespace DashboardCode.AdminkaV1.LoggingDom.DataAccessEfCore.OdataToEntity
+{
+    /// <summary>
+    /// OdataToEntity can't be referenced in Standard lib (only in .NET Framework)
+    /// More information: https://github.com/voronov-maxim/OdataToEntity
+    /// </summary>
 
-//        public async void AppendStream(Stream responceStream, Uri query)
-//        {
-//            //Query
-//            var uri = new Uri("http://dummy/Users?$select=Name");
-//            //The result of the query
-//            //Execute query
-//            await parser.ExecuteGetAsync(query, OeRequestHeaders.Default, responceStream, CancellationToken.None);
-//        }
-//    }
-//}
+    public class AdminkaODataContext
+    {
+        readonly OeParser parser;
+        public AdminkaODataContext(string baseUri, IDbContextOptionsFactory optionsFactory, Action<string> verbose)
+        {
+            var dataAdapter = new AdminkaOeEfCoreDataAdapter(optionsFactory, verbose);
+            //Build OData Edm Model
+            EdmModel edmModel = dataAdapter.BuildEdmModel();
+            //Create query parser
+            this.parser = new OeParser(new Uri(baseUri /*"http://dummy"*/), edmModel);
+        }
+
+        public void AppendStream(Stream responceStream, Uri query)
+        {
+            //Query
+            var uri = new Uri("http://dummy/Users?$select=Name");
+            //The result of the query
+            //Execute query
+            parser.ExecuteGetAsync(query, OeRequestHeaders.JsonDefault, responceStream, CancellationToken.None);
+        }
+    }
+}
---
+++ AdminkaV1/DataAccessEfCore.OdataToEntity/AdminkaOeEfCoreDataAdapter.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using OdataToEntity.EfCore;
+using DashboardCode.Routines.Storage.EfCore;
+
+namespace DashboardCode.AdminkaV1.LoggingDom.DataAccessEfCore.OdataToEntity
+{
+    public class AdminkaOeEfCoreDataAdapter : OeEfCoreDataAdapter<LoggingDomDbContext>
+    {
+        readonly IDbContextOptionsFactory optionsFactory;
+        readonly Action<string> verbose;
+        public AdminkaOeEfCoreDataAdapter(IDbContextOptionsFactory optionsFactory, Action<string> verbose)
+        {
+            this.optionsFactory = optionsFactory;
+            this.verbose = verbose;
+        }
+
+        public override object CreateDataContext()
+        {
+            return new LoggingDomDbContext((b) => optionsFactory.Create(b), verbose);
+        }
+    }
+}
